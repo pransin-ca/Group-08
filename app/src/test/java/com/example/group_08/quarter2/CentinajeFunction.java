623f84135d06b0b34b0f668644008b82f4768481
pransin-ca <26-000020@mcstayuman.edu.ph>
2026-09-04 17:10:03 +0800

    I have added a new file called "CentinajeFunction" where my personal information are stored.

---
+++ app/src/test/java/com/example/group_08/quarter2/CentinajeFunction.java
@@ -0,0 +1,21 @@
+package com.example.group_08.quarter2;
+
+import org.junit.Test;
+
+public class CentinajeFunction {
+
+    @Test
+
+    public void printMyProfile() {
+
+        String myName = "Franzen Kyle";
+        String petName = "Patchie";
+        String favFood = "Sinigang";
+        int myAge = 15;
+
+        System.out.println("--- MY DIGITAL PROFILE ---");
+        System.out.println("Hello, my name is " + myName + " and I am " + myAge + " years old.");
+        System.out.println("I have a wonderful pet named " + petName);
+        System.out.println("If I could, I would eat " + favFood + " every single day!");
+    }
+}
