8c0f0745efa3716a73d9a9ed7b5fa51e74019b80
pransin-ca <26-000020@mcstayuman.edu.ph>
2026-09-11 14:56:58 +0800

    Added code to CentinajeFunction.

--- app/src/test/java/com/example/group08/quarter2/CentinajeFunction.java
+++ app/src/test/java/com/example/group08/quarter2/CentinajeFunction.java
@@ -1,21 +1,19 @@
-package com.example.group_08.quarter2;
+package com.example.group08.quarter2;
 
 import org.junit.Test;
-
 public class CentinajeFunction {
 
     @Test
-
     public void printMyProfile() {
 
-        String myName = "Franzen Kyle";
+        String myName = "Franzen";
         String petName = "Patchie";
         String favFood = "Sinigang";
         int myAge = 15;
 
         System.out.println("--- MY DIGITAL PROFILE ---");
         System.out.println("Hello, my name is " + myName + " and I am " + myAge + " years old.");
-        System.out.println("I have a wonderful pet named " + petName);
-        System.out.println("If I could, I would eat " + favFood + " every single day!");
+        System.out.println("I have a wonderful pet named " + petName + ".");
+        System.out.println("If I could, I would eat " + favFood + " every single day.");
     }
 }
