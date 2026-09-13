925701acea8428cb6c34ad49bbb3ef1766598ce4
pransin-ca <26-000020@mcstayuman.edu.ph>
2026-09-13 15:48:51 +0800

    Made a new class for Belo and Francisco Gab.

---
+++ app/src/test/java/com/example/group08/quarter2/BeloFunction.java
@@ -0,0 +1,20 @@
+package com.example.group08.quarter2;
+
+import org.junit.Test;
+
+public class BeloFunction {
+
+    @Test
+    public void printMyProfile() {
+
+        String myName = "John Lawrence";
+        String petName = "Meatball";
+        String favFood = "Pork Adobo";
+        int myAge = 18;
+
+        System.out.println("--- MY DIGITAL PROFILE ---");
+        System.out.println("Hello, my name is " + myName + " and I am " + myAge + " years old.");
+        System.out.println("I have a wonderful pet named " + petName + ".");
+        System.out.println("If I could, I would eat " + favFood + " every single day.");
+    }
+}
