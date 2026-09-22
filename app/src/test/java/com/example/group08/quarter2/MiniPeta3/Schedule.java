9c97531fb6c064929cce398f510a6ffce5e8d374
calvinkleindejesus-ship-it <calvinkleindejesus@gmail.com>
2026-09-22 13:26:50 +0800

    Initial commit

--- app/src/test/java/com/example/group08/quarter2/MiniPeta3/Schedule.java
+++ app/src/test/java/com/example/group08/quarter2/MiniPeta3/Schedule.java
@@ -1,10 +1,17 @@
 package com.example.group08.quarter2.MiniPeta3;
 
-.out.printIn();
-system.out.printIn("/n answer your quizzes.");
-system.out.printIn("Show Subjects:");
+public class Schedule {
+    String day1 = "Monday - Math, Science, PE";
+    String day2 = "Tuesday - English, History, Art";
+    String day3 = "Wednesday - Math, Science, Music,";
+    String day4 = "Thursday - English, History, PE";
+    String day5 = "Friday - Math, Science, Art";
 
-system.out.printIn("---English---");
-system.out.printIn("---Math---");
-system.out.printIn("---Science---");
-system.out.printIn('---Cled---");
+    public void viewSchedule() {
+        System.out.println("=== SCHEDULE ===");
+        System.out.println(day1);
+        System.out.println(day2);
+        System.out.println(day3);
+        System.out.println(day4);
+        System.out.println(day5);
+}
