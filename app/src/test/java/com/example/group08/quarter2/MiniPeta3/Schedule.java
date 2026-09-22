fee1a540d2c184fa349f675d640edd9339987b78
Arvin <sophiee.0013@gmail.com>
2026-09-22 13:29:41 +0800

    Added a MainMenuTesting file with codes in it.

--- app/src/test/java/com/example/group08/quarter2/MiniPeta3/Schedule.java
+++ app/src/test/java/com/example/group08/quarter2/MiniPeta3/Schedule.java
@@ -1,17 +1,6 @@
 package com.example.group08.quarter2.MiniPeta3;
 
 public class Schedule {
-    String day1 = "Monday - Math, Science, PE";
-    String day2 = "Tuesday - English, History, Art";
-    String day3 = "Wednesday - Math, Science, Music,";
-    String day4 = "Thursday - English, History, PE";
-    String day5 = "Friday - Math, Science, Art";
-
-    public void viewSchedule() {
-        System.out.println("=== SCHEDULE ===");
-        System.out.println(day1);
-        System.out.println(day2);
-        System.out.println(day3);
-        System.out.println(day4);
-        System.out.println(day5);
+    String day1 = "MONDAY - Math, Science, PE";
+    String day2 = "TUESDAY - English, Science, AP";
 }
