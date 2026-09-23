ed6974fb2e12993d22720c1b381910e89729efe1
SHS PC <aeusmendoza@gmail.com>
2026-09-23 13:56:45 +0800

    Finished coding the profile section

--- app/src/test/java/com/example/group08/quarter2/MiniPeta3/Profile.java
+++ app/src/test/java/com/example/group08/quarter2/MiniPeta3/Profile.java
@@ -1,13 +1,14 @@
 package com.example.group08.quarter2.MiniPeta3;
 
+import java.util.Scanner;
+
 public class Profile {
     public void viewProfile(String name, String gradeAndSection, String adviser, String studentID, int age) {
         System.out.println("=== PROFILE ===");
         System.out.println("Name: " + name);
         System.out.println("Grade and Section: " + gradeAndSection);
         System.out.println("Adviser: " + adviser);
-        System.out.println("Student ID: " + studentID);
+        System.out.println("StudentID: " + studentID);
         System.out.println("Age: " + age);
-        System.out.println("=======");
     }
 }
