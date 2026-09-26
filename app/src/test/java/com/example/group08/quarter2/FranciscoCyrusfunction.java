152d7efe403f66f9ec3a474152daf797f88fbd81
Cyrus <cyrusjamesfrancisco2@gmail.com>
2026-09-26 15:41:59 +0800

    Added FranciscoCyrusfunction

---
+++ app/src/test/java/com/example/group08/quarter2/FranciscoCyrusfunction.java
@@ -0,0 +1,20 @@
+package com.example.group08.quarter2;
+
+import org.junit.Test;
+
+public class FranciscoCyrusfunction {
+
+    @Test
+    public void printMyProfile() {
+
+        String myName = "Cyrus";
+        String petName = "Timer";
+        String favFood = "Sinigang";
+        int myAge = 15;
+
+        System.out.println("--- MY DIGITAL PROFILE ---");
+        System.out.println("Hello, my name is " + myName + " and I am " + myAge + " years old.");
+        System.out.println("I have a wonderful pet named " + petName + ".");
+        System.out.println("If I could, I would eat " + favFood + " every single day.");
+    }
+}
